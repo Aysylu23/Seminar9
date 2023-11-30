3fd059b74d4da2e3aadb947dbd29bb8fdc35b85c
Aysylu <aysylu_bad@mail.ru>
2023-11-30 16:21:04 +0500

    Add task1

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"*/
+
+
+int numberM = ReadInt("Введите число M: ");
+int numberN = ReadInt("Введите число N: ");
+
+int PrintNumbers(int M, int N)
+{
+    if(M == N)
+       Console.WriteLine(M);
+       return;
+    else 
+       
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
