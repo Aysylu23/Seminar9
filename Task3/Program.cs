a4711bef800e8061b9d3224d909c90c4d364c052
Aysylu <aysylu_bad@mail.ru>
2023-11-30 16:21:29 +0500

    Add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = ReadInt("Введите число: ");
+Console.WriteLine(SumOfDigits(number));
+
+int SumOfDigits(int number)
+{
+    if(number == 0)
+        return 0;
+
+    int digit = number % 10;
+    int nextDigit = SumOfDigits(number / 10);
+    return digit + nextDigit;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
